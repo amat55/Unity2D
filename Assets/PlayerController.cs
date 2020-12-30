eddfe25d84fe64feb9a831dc1c2d12269b9afcb9
ahmet <41929175+amat55@users.noreply.github.com>
2020-12-30 23:51:28 +0100

    Initial commit

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class PlayerController : MonoBehaviour
+{
+    //Start() Variables
+    private Rigidbody2D rb;
+    private Animator anim;
+    private Collider2D coll;
+
+    //FSM
+    private enum State { idle,running,jumping,falling}
+    private State state = State.idle;
+
+    //Inseptor variables
+    [SerializeField] private LayerMask ground;
+    [SerializeField] private float speed = 5f;
+    [SerializeField] private float jumpForce = 10f;
+    [SerializeField] private int cherries = 0;
+    [SerializeField] private Text CherryText;
+    private void Start()
+    {
+        rb=GetComponent<Rigidbody2D>();
+        anim=GetComponent<Animator>();
+        coll = GetComponent<Collider2D>();
+    }
+
+    private void Update()
+    {
+        Movement();
+        AnimationState();
+        anim.SetInteger("state", (int)state);   //sets animation based on enumerator state
+    }
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+      if(other.tag=="collectable")
+        {
+            Destroy(other.gameObject);
+            cherries += 1;
+            CherryText.text = cherries.ToString();
+        }
+    }
+
+    private void Movement()
+    {
+        float _h = Input.GetAxis("Horizontal");
+
+        if (_h < 0)
+        {
+            rb.velocity = new Vector2(-speed, rb.velocity.y);
+            transform.localScale = new Vector2(-1, 1);
+            //anim.SetBool("running", true);
+        }
+        else if (_h > 0)
+        {
+            rb.velocity = new Vector2(speed, rb.velocity.y);
+            transform.localScale = new Vector2(1, 1);
+            //anim.SetBool("running", true);
+        }
+        else
+        {
+            //anim.SetBool("running", false);
+
+        }
+        if (Input.GetButtonDown("Jump") && coll.IsTouchingLayers(ground))
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+            state = State.jumping;
+        }
+    }
+
+    private void AnimationState()
+    {
+        if (state == State.jumping)
+        {
+            if (rb.velocity.y < .1f)
+            {
+                state = State.falling;
+            }
+        }
+        else if (state == State.falling)
+        {
+            if (rb.IsTouchingLayers(ground))
+            {
+                state = State.idle;
+            }
+        
+        }
+         else if (Mathf.Abs(rb.velocity.x) > 2f)
+        {
+            state = State.running;
+        }
+        else
+        {
+            state = State.idle;
+        }
+    }
+}
